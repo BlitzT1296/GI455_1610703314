7445f5a88df68c8175841ce1aa891d36a5068698
BlitzT12 <71089402+BlitzT12@users.noreply.github.com>
2021-01-29 17:12:42 +0700

    Send Assignment Week1

---
+++ GI455_Week1/Assets/Scripts/UI_Input.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_Input : MonoBehaviour
+{
+    public string[] names = new string [] {"Unity", "Unreal", "ResidentEvil", "Google", "MongoDB"};
+    public string output;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    
+
+    public void StoreInput()
+    {
+        output = inputField.GetComponent<Text>().text;
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (output == names[i])
+            {
+                textDisplay.GetComponent<Text>().text = output + " is Found";
+                return; 
+            }
+            else 
+            {
+                textDisplay.GetComponent<Text>().text = output + " is not Found";
+                
+            }
+
+        }
+        
+        //
+
+    }
+}
